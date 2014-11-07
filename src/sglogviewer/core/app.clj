a4e9c3c6f3f980f743f4d49770b520b6331cceda
Oleg Galbert <ogcraft@gmail.com>
2014-11-07 20:01:54 +0200

    ...

---
+++ src/sglogviewer/core/app.clj
@@ -0,0 +1,16 @@
+(ns sglogviewer.core.app
+  (:require [compojure.core :refer :all]
+            ))
+
+(def log-recs (atom {}))
+
+(def testlog1 "testdata/devMultiRack_SG_A.log")
+(def testlog2 "testdata/devMultiRack_SG_B.log")
+
+
+(defn add-to-log-recs [str]
+  )
+
+(defn read-file [fname]
+  (with-open [rdr (clojure.java.io/reader fname)]
+    (doall (map add-to-log-recs (line-seq rdr)))))
